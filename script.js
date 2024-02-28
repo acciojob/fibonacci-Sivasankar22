613d1c49a4855e0fc9d943023540d20bc38cf4ee
Sivasankar22 <83053496+Sivasankar22@users.noreply.github.com>
2024-02-28 22:35:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function fibonacci(num) {
-// your code here
+    if (num <= 0) {
+        return "Invalid input";
+    } else if (num === 1) {
+        return 0;
+    } else if (num === 2) {
+        return 1;
+    } else {
+        let prev = 0;
+        let current = 1;
+        for (let i = 3; i <= num; i++) {
+            let next = prev + current;
+            prev = current;
+            current = next;
+        }
+        return current;
+    }
 }
 
 module.exports = fibonacci;
